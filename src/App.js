2824b96e303dc4c399ccc3be6e27322e3bcb60bf
gustso <gustso@fi.unju.edu.ar>
2022-09-12 05:28:02 -0700

    tablero listo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { Container, Row } from 'react-bootstrap';
+import Cuadrado from './components/Cuadrado';
+
 function App() {
     return (
-        <><h1 className="display-1">HOLA</h1>
-        </>
+        <Container>        
+            <h1 className="display-1">HOLA</h1>        
+            <Row  xs="auto">        
+            <Cuadrado/>
+            <Cuadrado/>
+            <Cuadrado/>
+            </Row>        
+            <Row  xs="auto">        
+            <Cuadrado/>
+            <Cuadrado/>
+            <Cuadrado/>
+            </Row>        
+            <Row  xs="auto">        
+            <Cuadrado/>
+            <Cuadrado/>
+            <Cuadrado/>
+            </Row>        
+        </Container>
     );
   }
   
--- src/components/Cuadrado.js
+++ src/components/Cuadrado.js
@@ -1,4 +1,8 @@
-function Cuadrado () {
+import Button from 'react-bootstrap/Button';
 
+function Cuadrado () {
+    return (
+        <Button variant="outline-secondary">Light</Button>        
+    );
 }
 export default Cuadrado;
